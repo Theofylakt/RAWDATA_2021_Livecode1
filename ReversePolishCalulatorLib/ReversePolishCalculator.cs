1ff800089e84a74c51f497b2bd077efd08e7e01b
Henrik Bulskov <bulskov@ruc.dk>
2021-10-14 14:21:29 +0200

    Make Compute_AdditionWithTwoIntegers_ReturnTheSum test pass

--- ReversePolishCalulatorLib/ReversePolishCalculator.cs
+++ ReversePolishCalulatorLib/ReversePolishCalculator.cs
@@ -9,7 +9,15 @@ namespace ReversePolishCalculatorLib
             if (string.IsNullOrEmpty(input))
                 return 0;
 
-            return int.Parse(input);
+            var tokens = input.Split(' ');
+
+            if (tokens.Length == 1)
+                return int.Parse(input);
+
+            var val1 = int.Parse(tokens[0]);
+            var val2 = int.Parse(tokens[1]); ;
+
+            return val1 + val2;
         }
     }
 }
